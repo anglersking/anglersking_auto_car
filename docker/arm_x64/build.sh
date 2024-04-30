c0583a04c2f33e32bdf8db51df535b60c8d203e3
anglersking <418848424@qq.com>
2024-04-30 18:53:49 +0800

    remap dev arm64

--- docker/arm_x64/build.sh
+++ docker/arm_x64/build.sh
@@ -1,7 +1,7 @@
 docker rm -f anglersking_auto_car
 docker build -t anglersking_auto_car .
 
-docker run -dit --net=host --name anglersking_auto_car anglersking_auto_car
+docker run -dit  -v /dev:/dev --privileged=true  --net=host --name anglersking_auto_car anglersking_auto_car
 # --no-cache 
 #  --ulimit nofile=262144:262144 -p 9000:9000 
 
