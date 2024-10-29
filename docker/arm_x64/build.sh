c818fd63c4244402f934db79b3db099c043760c8
wanshan.pang <418848424@qq.com>
2024-10-30 00:09:28 +0800

    update

--- docker/arm_x64/build.sh
+++ docker/arm_x64/build.sh
@@ -1,5 +1,5 @@
 docker rm -f anglersking_auto_car
-docker build -t anglersking_auto_car .
+docker build -t anglersking_auto_car -f  ./dockerfile  ../../
 
 docker run -dit  -v /dev:/dev --privileged=true  --net=host --name anglersking_auto_car anglersking_auto_car
 # --no-cache 
---
+++ docker/arm_x64/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd  /auto_car && source /opt/ros/melodic/setup.bash  && catkin_make
