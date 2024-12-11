81aad3dcdfc31c4888f3ed2ff3529c2395649970
wanshan <418848424@q qq.com>
2024-12-12 01:18:34 +0800

    update arm64

---
+++ docker/arm_x64/build_car.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd  /auto_car && ls && source /opt/ros/melodic/setup.bash  && catkin_make -j100
--- docker/arm_x64/entrypoint.sh
+++ docker/arm_x64/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd  /auto_car && source /opt/ros/melodic/setup.bash  && catkin_make
+cd  /auto_car && source /opt/ros/melodic/setup.bash  && catkin_make -j100
